79111bdcbd57becf81e3060214d5df141ca2d0db
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-06-21 15:24:22 -0300

    save

---
+++ Seção03CreatingDatabases&Tables/03DroppingAndUsingDatabases.sql
@@ -0,0 +1,5 @@
+drop database chicken_coop
+// vai excluir o banco de dados
+//não usa drop com uma tabela com dados em produção
+
+SELECT database()
---
+++ Seção03CreatingDatabases&Tables/04CreatingTables.sql
@@ -0,0 +1,12 @@
+use pet_shop
+
+CREATE TABLE cats (
+    name varchar(50),
+    age int
+)
+
+create table dogs(
+	name varchar(50),
+  breed varchar(50),
+  age int
+)
